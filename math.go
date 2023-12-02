ea09e99c5cef226f4b4e1a0dfe6b0c57fe599108
logicsculptor0117 <logicsculptor0117@gmail.com>
2023-12-02 04:13:27 -1100

    Fixed bug

--- math.go
+++ math.go
@@ -46,9 +46,9 @@ func (d Decimal) PowFrac(num, denom int) Decimal {
 
 // Max returns the value closest to positive infinity.
 func Max(first Decimal, others ...Decimal) Decimal {
-	for _, dd := range others {
-		if first.LessThan(dd) {
-			first = dd
+	for _, d := range others {
+		if first.LessThan(d) {
+			first = d
 		}
 	}
 	return first
